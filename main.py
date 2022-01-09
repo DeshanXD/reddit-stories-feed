264daad6ab2e6f38435591ae1178dc6d9829f99a
DeshanXD <basuradeshan@gmail.com>
2022-01-09 10:32:19 +0530

    Added main file [install dependancies]

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+import subprocess
+import os
+
+
+def install_dependancies():
+    subprocess.check_call(['apt', 'install', 'git']) # use to to auto commits
+    subprocess.check_call([sys.executable, '-m' , 'pip', 'install',  'requests'])
+    subprocess.check_call([sys.executable, '-m' , 'pip', 'install',  'discord-webhook']) # Request is dependancy of discord-webhook
+
+
+def main():
+    install_dependancies()
+    
+
+if __name__ == "main":
+    main()
+    
